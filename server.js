b2548d734faf7cd3ae4db0944ccfe166437fb1ec
Benjamin Dolan <benjamin@thinkful.com>
2020-02-19 07:50:08 -0700

    added the handleGetPokemon route/endpoint

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+
 console.log(process.env.API_TOKEN);
+
 const app = express();
 app.use(morgan('dev'));
 
@@ -11,6 +14,12 @@ res.json(validTypes);
 }
 
 app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+  res.send('Hello, Pokemon!');
+}
+
+app.get('/pokemon', handleGetPokemon);
 
 const PORT = 8000;
 
